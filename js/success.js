3d70620118d72c103e249ec24c7493a7139b125d
yao-bei-bei <31236717+yao-bei-bei@users.noreply.github.com>
2017-11-02 18:19:32 +0800

    1

--- js/success.js
+++ js/success.js
@@ -1,57 +1,121 @@
+app.controller('siteCtrl', function ($scope, $http, $state,$filter) {
+    //列表
+    $scope.demoLists = [];
+    $scope.pagination = [];
 
-app.controller('siteCtrl', function($scope, $http) {
-	$scope.article={
-		ajaxData:[]
-	}
-	$http({
-		method: 'GET',
-		url: '/carrots-admin-ajax/a/article/search'
-	}).then(function(response) {
-			$scope.article.ajaxData = response.data.data.articleList;
-			console.log($scope.article.ajaxData)
-		}, function(response) {
-			alert(1)
-	});
+    $http({
+        method: 'GET',
+        url: '/carrots-admin-ajax/a/article/search'
+    }).then(function (response) {
+        //列表
+        $scope.demoLists = response.data.data.articleList;//表格
+        console.log($scope.demoLists)
+        $scope.pagination=response.data.data;
+        $scope.size=$scope.pagination.size;
+        $scope.pagina = Math.ceil($scope.pagination.total / 10);//页数量
+        $scope.pagination.page = $scope.pagina;
+        //按照每页显示10个数据，得到总页数
+        $scope.pageNum = [];                                //生成页码，在 html里 ng-repeat 出来
+        for (var i = 1; i <= $scope.pagina; i++) {
+            $scope.pageNum.push(i);
+        };
+        $scope.currentPage = 1;
+        //设置当前页是 0
+
+
+        $scope.pickerParams= {
+            startAt:"",
+            endAt:''
+        }
+            $scope.params={
+                page:$scope.currentPage,
+                startAt:'',
+                endAt:'',
+                type:'',
+                status:''
+            };
+
+        $scope.res= function () {
+            var promise= $http({
+                method: 'GET',
+                url: '/carrots-admin-ajax/a/article/search',
+                params:$scope.params,
+            });
+            promise.then(function (response) {
+                if (response.data.code == 0) {
+                    $scope.demoLists = response.data.data.articleList;//表格
+                    $state.go($state.current, {reload: true});
+                }
+            }, function () {
+                alert('错误')
+            })
+        };
+        $scope.res();
+        $scope.setPage = function (nam) {             // 当点击页码数字时执行的函数
+            $scope.currentPage = nam;
+            console.log($scope.currentPage);
+            if (nam <= $scope.pagina) {
+                $scope.demoLists = response.data.data.articleList;//表格
+            } else {
+                return;
+            };
+            // alert(nam)//将当前页 设置为 页码数
+            $scope.res();
+        };
+        $scope.prevPage = function () {               //点击上一页执行的函数
+            if ($scope.currentPage > 0) {
+                $scope.currentPage--;
+                $scope.num = $scope.currentPage;
+                $scope.res();
+            }
+        };
+        $scope.nextPage = function () {              //点击下一页执行的函数
+            if ($scope.currentPage < $scope.pagina) {
+                $scope.currentPage++;
+                $scope.num = $scope.currentPage;
+                $scope.res($scope.params);
+            }
+        }
+        //搜索
+        $scope.search =function () {
+            $scope.params.startAt=$scope.pickerParams.startAt&& Date.parse($scope.pickerParams.startAt);
+            $scope.params.endAt=$scope.pickerParams.endAt&&Date.parse($scope.pickerParams.endAt);
+            $scope.res();
+        }
+        $scope.searching=function () {
+            $scope.params=null;
+            $scope.demoLists = response.data.data.articleList;//表格
+        }
+    }, function (response) {
+        alert(1)
+    });
 
 });
-//	app.config(['$routeProvider', function($routeProvider) {
-//		$routeProvider
-//			.when('/computers', {
-//				templateUrl: 'page.html'
-//			})
-//			.when('/Article', {
-//				templateUrl: 'Article.html',
-//				controller:'siteCtrl'
-//			})
-//			.when('/add', {
-//				templateUrl: 'Add.html'
-//			})
-//			.otherwise({
-//				templateUrl: 'page.html'
-//			});
-//	}]);
-	//字段 类别 type  0-首页banner 1-找职位banner  2-找精英banner 3-行业大图
-	app.filter('type',function(){
-		return function(input){
-			if (input==0) {
-				input="首页banner"
-			} else if(input==1){
-				input="找职位banner"
-			}else if(input==2){
-				input="找精英banner"
-			}else if(input==3){
-				input="行业大图"
-			}
-			return input
-		}
-	});
-	app.filter('status',function(){
-		return function(input){
-			if(input==1){
-				input="草稿"
-			}else if(input==2){
-				input="上线 "
-			}
-			return input
-		}
-	});
+
+//字段 类别 type  0-首页banner 1-找职位banner  2-找精英banner 3-行业大图
+app.filter('type', function () {
+    return function (input) {
+        if (input == 0) {
+            input = "首页banner"
+        } else if (input == 1) {
+            input = "找职位banner"
+        } else if (input == 2) {
+            input = "找精英banner"
+        } else if (input == 3) {
+            input = "行业大图"
+        }
+        return input
+    }
+});
+app.filter('status', function () {
+    return function (input) {
+        if (input == 1) {
+            input = "草稿"
+        } else if (input == 2) {
+            input = "上线 "
+        }
+        return input
+    }
+});
+
+

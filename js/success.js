aaebf6ecc0898067517f21e21089011297e44d2c
yao-bei-bei <31236717+yao-bei-bei@users.noreply.github.com>
2017-11-03 16:19:32 +0800

    1

--- js/success.js
+++ js/success.js
@@ -3,93 +3,85 @@ app.controller('siteCtrl', function ($scope, $http, $state,$filter) {
     $scope.demoLists = [];
     $scope.pagination = [];
 
-    $http({
-        method: 'GET',
-        url: '/carrots-admin-ajax/a/article/search'
-    }).then(function (response) {
-        //列表
-        $scope.demoLists = response.data.data.articleList;//表格
-        console.log($scope.demoLists)
-        $scope.pagination=response.data.data;
-        $scope.size=$scope.pagination.size;
-        $scope.pagina = Math.ceil($scope.pagination.total / 10);//页数量
-        $scope.pagination.page = $scope.pagina;
-        //按照每页显示10个数据，得到总页数
-        $scope.pageNum = [];                                //生成页码，在 html里 ng-repeat 出来
-        for (var i = 1; i <= $scope.pagina; i++) {
-            $scope.pageNum.push(i);
-        };
+    $scope.pickerParams= {
+        startAt :"",
+        updateAt:'',
+    };
+    $scope.params={
+        page:1,
+        startAt :'',
+        endAt :'',
+        type:'',
+        status:'',
+        size:10,
+    };
 
-        $scope.currentPage =1;
-        console.log( $scope.pageNum)
-        //设置当前页是 0
-        $scope.pickerParams= {
-            startAt :"",
-            updateAt:'',
-        };
-            $scope.params={
-                page:$scope.currentPage,
-                startAt :'',
-                endAt :'',
-                type:'',
-                status:''
-            };
-
-        $scope.res= function () {
-            var promise= $http({
-                method: 'GET',
-                url: '/carrots-admin-ajax/a/article/search',
-                params:$scope.params,
-            });
-
-            promise.then(function (response) {
-                if (response.data.code == 0) {
-                    $scope.demoLists = response.data.data.articleList;//表格
-                }
-            }, function () {
-                alert('错误')
-            })
-        };
-        $scope.res();
-        $scope.setPage = function (nam) {             // 当点击页码数字时执行的函数
-            $scope.params.page = nam;
-            if (nam <= $scope.pagina) {
+    $scope.res= function () {
+        var promise= $http({
+            method: 'GET',
+            url: '/carrots-admin-ajax/a/article/search',
+            params:$scope.params,
+        });
+        promise.then(function (response) {
+            if (response.data.code == 0) {
                 $scope.demoLists = response.data.data.articleList;//表格
-            } else {
-                return;
-            };
-            // alert(nam)//将当前页 设置为 页码数
-            $scope.res();
-        };
-        $scope.prevPage = function () {               //点击上一页执行的函数
-            if ($scope.params.page > 0) {
-                $scope.params.page--;
-                $scope.num = $scope.params.page;
-                $scope.res();
-            }
-        };
-        $scope.nextPage = function () {              //点击下一页执行的函数
-            if ($scope.params.page < $scope.pagina) {
-                $scope.params.page++;
-                $scope.num = $scope.params.page;
-                $scope.res();
+                $scope.pagination=response.data.data;
+                $scope.pagina = Math.ceil($scope.pagination.total / $scope.params.size);//页数量
+                //生成页码，在 html里 ng-repeat 出来
+                $scope.pageNum = [];
+                for (var i = 1; i <=$scope.pagina; i++) {
+                    $scope.pageNum.push(i);
+                };
+
             }
         }
-        //搜索
-        $scope.search =function () {
-            $scope.params.startAt=$scope.pickerParams.startAt && Date.parse($scope.pickerParams.startAt );
-            $scope.params.endAt =$scope.pickerParams.endAt &&Date.parse($scope.pickerParams.endAt );
+        , function () {
+            alert('错误')
+        })
+    };
 
+    $scope.res();
+    $scope.setPage = function (num) {             // 当点击页码数字时执行的函数
+        $scope.params.page = num;
+        console.log(num)
+        $scope.res();
+
+        if (num <= $scope.pagina && num!=0) {
+            $scope.pagination.page=$scope.params.page;
             $scope.res();
+        } else {
+            return;
+        };
+        // alert(num)//将当前页 设置为 页码数
+    };
+    $scope.prevPage = function (num) {               //点击上一页执行的函数
+        if ($scope.page >=1) {
+            $scope.params.page--;
+            $scope.num= $scope.params.page;
+            $scope.res();
+        }else{
+            return;
         }
-        $scope.searching=function () {
-            $scope.params=null;
-            $scope.demoLists = response.data.data.articleList;//表格
+    };
+    $scope.nextPage = function (num) {              //点击下一页执行的函数
+        if ($scope.params.page < $scope.pagina) {
+            $scope.params.page++;
+            $scope.num = $scope.params.page;
+            $scope.res();
+        }else{
+            return;
         }
-    }, function (response) {
-        alert(1)
-    });
-
+    }
+    //搜索
+    $scope.search =function () {
+        $scope.params.startAt=$scope.pickerParams.startAt && Date.parse($scope.pickerParams.startAt );
+        $scope.params.endAt =$scope.pickerParams.endAt &&Date.parse($scope.pickerParams.endAt );
+        $scope.res();
+    }
+    $scope.searching=function () {
+        $scope.params=null;
+        $scope.res();
+    }
 });
 
 //字段 类别 type  0-首页banner 1-找职位banner  2-找精英banner 3-行业大图
